05b52dc2d0f4bcb40d3f9edff03b8cc92bf1a170
Urs <urs@ursb.de>
2021-10-22 19:32:28 +0200

    IP Adresse anzeigen lassen

--- detect/main.py
+++ detect/main.py
@@ -27,6 +27,13 @@ import time
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.WARNING)
 
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+s.close()
+
+
 # Routing
 @app.route('/')
 def home():
