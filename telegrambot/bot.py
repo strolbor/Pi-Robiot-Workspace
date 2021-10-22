20c7487aebf58b8f161e2bf7dc03754cef78418d
Urs Braun <urs@ursb.de>
2021-10-22 12:59:39 +0200

    Fix TBOT 2

--- telegrambot/bot.py
+++ telegrambot/bot.py
@@ -9,6 +9,7 @@ import os
 #pip install python-telegram-bot
 
 TELEGRAM_EMP_CONF = '/root/urs_robot/server/tg_empf.csv'
+#TELEGRAM_EMP_CONF = 'a.tmp'
 """Hier wird die ID des Empfängers der Nachricht hinterlegt."""
 
 def delete_file(filename):
@@ -27,32 +28,38 @@ def id(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f'Deine ID ist: {id}')
 
 def register(update: Update, context: CallbackContext) -> None:
+    line = ""
     try:
         # Datei lesen und verstehen
         datei = open(TELEGRAM_EMP_CONF,"r")
         line = datei.readline()
-        line = line.replace("\n","")
-        line = line.replace("\r","")
-        array = line.split(",")
+        #line = line.replace("\n","")
+       # line = line.replace("\r","")
         datei.close()
+        print(line)
         # ID des Telegram Nutzer herausfinden
-        id = str(update.effective_user.id)
-        print("Regsiterung von:",id,"...")
-
-        if id not in array:
-            # Registierung ist erfolgreich, weil er nicht in der Liste ist
-            delete_file(TELEGRAM_EMP_CONF)
-            datei = open(TELEGRAM_EMP_CONF,"a")
-            datei.write(line+","+id)
-            datei.flush()
-            datei.close()
-            update.message.reply_text(f'Du hast dich erfolgreich registiert. Du wirst zukünftig benachrichtig. Zum löschen dieser Registierung, gehe in Urs Dashboard unter der URL: /dashboard')
-        else:
-            # Registierung nicht erfolgreich, da er schon in der Liste ist
-            update.message.reply_text(f'Du bist schon registiert.')
     except FileNotFoundError:
-        print("FAIL")
-        update.message.reply_text(f'Ich konnte dich leider nicht registiren, da Fehler passiert ist.')
+        pass
+
+    id = str(update.effective_user.id)
+    array = line.split(",")
+    print("Registerung von:",id,"...")
+    if id not in array:
+        # Registierung ist erfolgreich, weil er nicht in der Liste ist
+        delete_file(TELEGRAM_EMP_CONF)
+        datei = open(TELEGRAM_EMP_CONF,"a")
+        if len(line) == 0:
+            datei.write(id)
+        else:
+            datei.write(line+","+id)
+        datei.flush()
+        datei.close()
+        update.message.reply_text(f'Du hast dich erfolgreich registiert. Du wirst zukünftig benachrichtig. Zum löschen dieser Registierung, gehe in Urs Dashboard unter der URL: /dashboard')
+        print("Erfolgreich")
+    else:
+       # Registierung nicht erfolgreich, da er schon in der Liste ist
+        update.message.reply_text(f'Du bist schon registiert.')
+        print("nicht erfolgreich")
     
 
 updater = Updater('2050496929:AAHmHvQuPsPBqm4CqWj-QRPcGNksvk8BMDY')
